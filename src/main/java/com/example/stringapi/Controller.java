790bf579bafdc714ead887fefb4eef0d03600631
kamilb22 <222574@edu.p.lodz.pl>
2021-10-21 14:56:19 +0200

    Implementation of lowerCase endpoint

--- src/main/java/com/example/stringapi/Controller.java
+++ src/main/java/com/example/stringapi/Controller.java
@@ -1,11 +1,20 @@
 package com.example.stringapi;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class Controller {
-
+    @RequestMapping("/lowerCase/{string}")
+    public int lowerCase(@PathVariable String string){
+        int number = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLowerCase(string.charAt(i))) number++;
+        }
+        return number;
+    }
 
 }
